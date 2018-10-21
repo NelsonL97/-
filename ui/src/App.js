1ac7a79387e76f4ccff1b4d8bb8b444aa4dcc44d
Ameer Sami <ameersami@Ameers-MacBook-Pro.local>
2018-10-20 22:09:23 -0400

    Added stuff boi

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,16 +1,75 @@
 import React, { Component } from 'react';
-import './App.css';
+import styled from 'styled-components';
+import css from './app.css';
+import space from './space.jpg';
+import SpeechRecognition from 'react-speech-recognition'
+import gameboi from './GameBoi.png'
+
+const AppBackground = styled.div`
+  background: url(${space});
+  background-size: cover;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  display: flex;          
+  flex-direction: column;  
+  justify-content: center; 
+  align-items: center;     
+  height: 100vh;
+  color: white;
+`;
+
+const LargeText = styled.span`
+  font-size: 100px;
+`;
 
 class App extends Component {
   render() {
+    const { transcript, resetTranscript, startListening, browserSupportsSpeechRecognition, recognition } = this.props;
+    recognition.lang="en"
+    // recognition.onend = () => {startListening()};
+    console.log(recognition);
+    if (!browserSupportsSpeechRecognition) {
+      return (<p>Your browser doesn't allow speech recognition</p>)
+    }
     return (
-      <div className="App">
-        <h1>
-          YEA BOI
-        </h1>
-      </div>
+      <AppBackground>
+        <img src={gameboi}/>
+        {/* {console.log(this.props)} */}
+        <LargeText>
+          {/* YEA BOI */}
+          {this.props.interimTranscript}
+        </LargeText>
+      </AppBackground>
     );
   }
 }
 
-export default App;
+export default SpeechRecognition(App);
+
+
+// window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+// const recognition = new SpeechRecognition();
+// recognition.interimResults = true;
+
+// let p = document.createElement('p');
+// const words = document.querySelector('.words');
+// words.appendChild(p);
+
+//   recognition.addEventListener('result', e => {
+//     console.log(e.results)
+//     const transcript = Array.from(e.results)
+//     .map(result => result[0])
+//     .map(result => result.transcript)
+//     .join(''); 
+
+//     p.textContent = transcript;
+//     if(e.results[0].isFinal){
+//       p = document.createElement('p');
+//       words.appendChild(p);
+//     }      
+//     // console.log(transcript);
+//   });
+//   recognition.addEventListener('end', recognition. start)
+//   recognition.start();
